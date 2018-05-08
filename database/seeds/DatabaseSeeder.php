a48aea9417f456fda42d1202a7f592a0a81d233d
Que Pham <tinhbanbonphuong126@gmail.com>
2018-05-09 01:16:54 +0700

    QP: First initial Github

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//         $this->call(UsersTableSeeder::class);
+
+        factory(User::class, 1)->create();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+
+//All route for frontend
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//All Route for Admin Management
+Route::group(['prefix' => 'admin'], function() {
+
+    Route::auth();
+
+    Route::group(['middleware' => 'auth'], function () {
+
+        Route::get('/', function () {
+            return view('welcome');
+        });
+
+    });
+});
